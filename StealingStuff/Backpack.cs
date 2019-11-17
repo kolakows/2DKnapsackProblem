941fd07133c6085c9c527cf0616e4d7fcf1784d0
Igor Kołakowski <kolakowski.igor@gmail.com>
2019-11-17 19:25:13 +0100

    Add project files.

---
+++ StealingStuff/Backpack.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealingStuff
+{
+    public class Backpack
+    {
+        private Placement[] Placements { get; set; }
+        private ItemStatus[] Status { get; set; }
+        private bool[,] SpotTaken { get; set; }
+        private List<Item> Items { get; set; }
+        public double TotalValue { get; set; }
+        public int R { get; set; }
+        
+
+        public int TotalItemsArea { get; set; }
+
+        public readonly int Width;
+        public readonly int Height;
+        public readonly int Area;
+
+        public Backpack(int width, int height,int capacity)
+        {
+            SpotTaken = new bool[width, height];
+            Status = new ItemStatus[capacity];
+            for (int i = 0; i < Status.Length; i++)
+                Status[i] = ItemStatus.NotChecked;
+            Placements = new Placement[capacity];
+            Items = new List<Item>();
+            R = -1;
+            Width = width;
+            Height = height;
+            Area = Width * Height;
+        }
+
+        public void BecomeCopyOf(Backpack other)
+        {
+            Array.Copy(other.Placements, Placements, other.Placements.Length);
+            Array.Copy(other.Status, Status, other.Status.Length);
+            Array.Copy(other.SpotTaken, SpotTaken, other.SpotTaken.Length);
+            Items = other.Items.ToList();
+            TotalValue = other.TotalValue;
+            TotalItemsArea = other.TotalItemsArea;
+        }
+
+        public void Add(Item item, int width, int height, bool rotation)
+        {
+            Items.Add(item);
+            TotalValue += item.Value;
+            TotalItemsArea += item.Width * item.Height;
+            Placements[Items.Count - 1].x = width;
+            Placements[Items.Count - 1].y = height;
+            Placements[Items.Count - 1].rotation = rotation;
+
+        }
+
+        public void Pop()
+        {
+            if(Items.Count > 0)
+            {
+                var item = Items[Items.Count - 1];
+                if(Status[Items.Count-1] == ItemStatus.Placed)
+                    TakeItemOff(Items.Count - 1);
+                Status[Items.Count - 1] = ItemStatus.NotChecked;
+                TotalValue -= item.Value;
+                TotalItemsArea -= item.Height * item.Width;
+                Items.RemoveAt(Items.Count - 1); //musi być na końcu, edytuje indeks
+            }
+        }
+
+        public bool PlaceItem(int ind)
+        {
+            var item = Items[ind];
+            var itemWidth = Placements[ind].rotation ? item.Height : item.Width;
+            var itemHeight = Placements[ind].rotation ? item.Width : item.Height;
+
+            if (Placements[ind].x + itemWidth - 1 >= Width || Placements[ind].y + itemHeight - 1 >= Height)
+                return false;
+
+            for (int i = 0; i < itemWidth; i++)
+                for (int j = 0; j < itemHeight; j++)
+                    if (SpotTaken[Placements[ind].x + i, Placements[ind].y + j])
+                        return false;
+
+            for (int i = 0; i < itemWidth; i++)
+                for (int j = 0; j < itemHeight; j++)
+                    SpotTaken[Placements[ind].x + i, Placements[ind].y + j] = true;
+            return true;
+        }
+
+        public bool FeasibilityCheck()
+        {
+            if (Status[Items.Count - 1] == ItemStatus.Placed)
+                return true;
+            for (int i = 0; i < Items.Count; i++)
+            {
+                switch (Status[i])
+                {
+                    case ItemStatus.Placed:
+                        continue;
+                    case ItemStatus.DoesntFit:
+                        return false;
+                    case ItemStatus.NotChecked:
+                        if (PlaceItem(i))
+                        {
+                            Status[i] = ItemStatus.Placed;
+                            continue;
+                        }     
+                        else
+                        {
+                            Status[i] = ItemStatus.DoesntFit;
+                            return false;
+                        }
+                }
+            }
+            return true;
+        }
+
+        private void TakeItemOff(int ind)
+        {
+            var item = Items[ind];
+            var itemWidth = Placements[ind].rotation ? item.Height : item.Width;
+            var itemHeight = Placements[ind].rotation ? item.Width : item.Height;
+            for (int i = 0; i < itemWidth; i++)
+                for (int j = 0; j < itemHeight; j++)
+                    SpotTaken[Placements[ind].x + i, Placements[ind].y + j] = false;
+        }
+    }
+
+    public enum ItemStatus
+    {
+        NotChecked,
+        Placed,
+        DoesntFit
+    }
+
+    public struct Placement
+    {
+        public int x;
+        public int y;
+        public bool rotation;
+    }
+}
---
+++ StealingStuff/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealingStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allLines = System.IO.File.ReadAllLines("./test2.txt");
+            int width = Int32.Parse(allLines[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0]), height = Int32.Parse(allLines[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1]);
+            int numberOfItems = Int32.Parse(allLines[1].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+            var allItems = new List<Item>();
+            for (int i = 2; i < allLines.Length; i++)
+            {
+                var info = allLines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                allItems.Add(new Item()
+                {
+                    Height = Int32.Parse(info[0]),
+                    Width = Int32.Parse(info[1]),
+                    Value = Int32.Parse(info[2])
+                });
+            }
+            var backpack = new Backpack(width, height, numberOfItems);
+            var bestSolution = new Backpack(width, height, numberOfItems);
+            var sortedItems = allItems.OrderByDescending(x => x.Value / ((double)x.Height * (double)x.Width)).ToList();
+            StealSomeMore(backpack, bestSolution, sortedItems);
+        }
+
+        static void StealSomeMore(Backpack current, Backpack best, List<Item> allItems)
+        {
+            if (current.TotalItemsArea > current.Area) //na pewno nie da się zapakować
+                return;
+            if(current.TotalValue > best.TotalValue)
+            {
+                if (current.FeasibilityCheck())
+                {
+                    best.BecomeCopyOf(current);
+                }
+            }
+            if (current.R >= allItems.Count - 1) //nie ma więcej przedmiotów do dodania
+                return;
+            for (int r = current.R+1; r < allItems.Count; r++)
+            {
+                for (int i = 0; i < current.Width; i++)
+                {
+                    for (int j = 0; j < current.Height; j++)
+                    {
+                        for (int k = 0; k < 2; k++)
+                        {
+                            current.R = r;
+                            current.Add(allItems[r], i, j, k == 1);
+                            StealSomeMore(current, best, allItems);
+                            current.Pop();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
